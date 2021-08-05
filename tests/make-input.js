e10a9cdfb63b5e3e3e3d340ac11a2d9b2a7a4c3a
Pacharapol Withayasakpunt <polv@polv.cc>
2021-08-05 21:04:31 +0700

    rename files

--- tests/make-input.js
+++ tests/make-input.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 const { app, Menu } = require('electron')
 const yaml = require('js-yaml')
@@ -6,6 +7,7 @@ const yaml = require('js-yaml')
 async function main() {
     await app.whenReady()
     fs.writeFileSync('menu.yml', yaml.dump(Menu.getApplicationMenu(), { skipInvalid: true }))
+    fs.copyFileSync('menu.yml', path.join(__dirname, `menu-${process.platform}.yml`))
     app.quit()
 }
 
